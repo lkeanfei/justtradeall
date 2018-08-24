a1ac76d9c1ccee0e52d7f43687da0b92ebac825e
lkeanfei <lkeanfei@gmail.com>
2018-08-24 18:00:19 +0800

    1. Added Sub titles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  marketTitles = ['Market Summary'];
+  analysisTitles = ['Shareholdings' , 'Stock Quotes']
+  secondaryTitles = [ '1' ,'222' , '333444'];
+
+  activateMarket() {
+    console.log("Activate Market!");
+    this.secondaryTitles = [];
+    for( const data of this.marketTitles) {
+      this.secondaryTitles.push(data);
+    }
+
+  }
+
+  activateAnalysis() {
+    console.log('Activate Analysis');
+    this.secondaryTitles = [];
+    for( const data of this.analysisTitles) {
+      this.secondaryTitles.push(data);
+    }
+  }
+
+  testClick(param: string) {
+    console.log('param is ' + param);
+  }
+
 }
