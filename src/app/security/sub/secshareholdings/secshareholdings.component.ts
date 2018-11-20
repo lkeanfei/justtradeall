2ab45ff73de9e0ba221d04cffc067176c474e9e9
keanfei <lkeanfei@gmail.com>
2018-11-20 21:30:02 +0800

    Merge branch 'master' of https://github.com/lkeanfei/justtradeall
    # Conflicts:
    #	src/app/security/security.component.html
    #	src/app/security/sub/overview/overview.component.html

--- src/app/security/sub/secshareholdings/secshareholdings.component.ts
+++ src/app/security/sub/secshareholdings/secshareholdings.component.ts
@@ -3,6 +3,22 @@ import {HttpService} from "../../../shared/httpservice.service";
 import {Observable} from "rxjs/index";
 import {concatMap } from 'rxjs/operators';
 import {ActivatedRoute} from "@angular/router";
+import {MatTableDataSource} from '@angular/material';
+import {CompanyData} from '../../../shareholdings/shareholdings.component';
+
+export interface DistHoldingsData {
+  levelname: string;
+  numshareholders: number;
+  numshares: number;
+  percentage: number;
+}
+
+export interface ShareHolderData {
+  id: number;
+  detailname: string;
+  shares: number;
+  percentage: number;
+}
 
 @Component({
   selector: 'app-secshareholdings',
@@ -13,8 +29,15 @@ export class SecshareholdingsComponent implements OnInit {
 
 
   showLogin: boolean;
+  distholdingsColumns = ['levelname', 'numshareholders', 'numshares', 'sharespercentage'];
+  Top30Columns =  ['id', 'detailname', 'shares', 'percentage'];
+  distholdingsDataSource: any;
+  top30DataSource: any;
 
   constructor(private route: ActivatedRoute ,private httpService: HttpService) {
+
+    this.distholdingsDataSource = new MatTableDataSource<DistHoldingsData>( );
+    this.top30DataSource = new MatTableDataSource<ShareHolderData>();
 
     const theSub = this.route.params.pipe(
       concatMap( prms => this.routeChangedDetected(prms))
@@ -31,8 +54,13 @@ export class SecshareholdingsComponent implements OnInit {
       } else {
         this.showLogin = false;
         console.log(res['top30year']);
-        console.log(res['distlist']);
+        // console.log(res['distlist']);
         console.log(res['top30list']);
+
+
+
+        this.distholdingsDataSource.data = res['distlist'];
+        this.top30DataSource.data = res['top30list'];
 
       }
     })
