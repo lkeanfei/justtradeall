2eae62bd631a61292107681bb7362e75897ba1b3
keanfei <lkeanfei@gmail.com>
2019-03-12 17:31:07 +0800

    1. Added TradeSummarySignal

--- src/app/shareholdings/shareholdings.component.ts
+++ src/app/shareholdings/shareholdings.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormControl, FormGroup, FormBuilder} from '@angular/forms';
 import {HttpService} from '../shared/httpservice.service';
-import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {MatPaginator, MatSort, MatTableDataSource, Sort} from '@angular/material';
 import { AngularFirestore , AngularFirestoreCollection } from '@angular/fire/firestore';
 import {Observable} from "rxjs";
 
@@ -38,8 +38,21 @@ export interface OwnershipSummaryItem {
   PercentageSharesAcquired : number;
   SharesDisposed : number;
   PercentageSharesDisposed : number;
+}
 
+export interface TradeSummarySignalItem {
+  Symbol: string;
+  MostBuyUpPrice: number;
+  MostSellDownPrice: number;
+}
 
+export interface TradeSummarySignalDetailsItem {
+  Price: number ;
+  Trades: number;
+  BuyUp: number;
+  AverageBuyUpTrades: number;
+  Selldown: number;
+  AverageSellDownTrades: number;
 }
 
 @Component({
@@ -61,11 +74,16 @@ export class ShareholdingsComponent implements OnInit {
   holdersDataSource: any;
   equitiesTotalShareChangeDataSource: any;
   ownershipSummaryDataSource: any;
+  tradeSummarySignalDataSource: any;
+  tradeSummarySignalDetailsDataSource: any;
 
   companyColumns = ['id', 'name', 'shares', 'percentage'];
   holderColumns = ['id', 'holder' , 'name' ,  'shares' , 'percentage'];
   equitiesTotalShareChangeColumns = [ 'Symbol' , 'NumSharesDiff'  , 'PercentageDiff'];
   ownershipSummaryColumns = ['Symbol' , 'SharesAcquired' , 'PercentageSharesAcquired' , 'SharesDisposed' ,'PercentageSharesDisposed'];
+  tradeSummarySignalColumns = ['Symbol' , 'MostBuyUpPrice' , 'MostSellDownPrice'];
+  tradeSummarySignalDetailsColumns = [ 'Price' , 'Trades' , 'BuyUp' , 'AverageBuyUpTrades' , 'Selldown' , 'AverageSellDownTrades'];
+
 
   options: FormGroup;
 
@@ -86,10 +104,27 @@ export class ShareholdingsComponent implements OnInit {
   ownershipSharesDisposedWidth = 23;
   ownershipPercentageSharesDisposedWidth = 23;
 
+  tradeSymbolWidth = 20;
+  mostBuyUpWidth = 40;
+  mostSellDownWidth = 40;
+
+  tradeSummaryDetailSymbol = '';
+
+  //Trade Symmary signal details table
+  priceWidth = 10;
+  tradesWidth = 10;
+  buyUpWidth = 20;
+  averageBuyUpTradesWidth = 20;
+  sellDownWidth = 20;
+  averageSellDownTradesWidth = 20;
+
   companySelectedYear = '2017';
   shareHolderSelectedYear = '2017';
   shareHolderTableLoading: boolean;
   startSearchingShareHolders: boolean;
+
+  mostBuyUpPrice: number;
+  mostSellDownPrice: number;
 
   // Styling for the loading spinner
   color = 'primary';
@@ -98,6 +133,8 @@ export class ShareholdingsComponent implements OnInit {
 
   @ViewChild('totalshare') equitiesTotalShareSort: MatSort;
   @ViewChild('ownership') ownershipSummarySort: MatSort;
+  @ViewChild('tradesummarysignal') tradesummarysignalSort: MatSort;
+  @ViewChild('tradesummarysignaldetails') tradeSummarySignalDetailsSort: MatSort;
 
 
 
@@ -114,6 +151,8 @@ export class ShareholdingsComponent implements OnInit {
     this.holdersDataSource = new MatTableDataSource<HoldersData>();
     this.equitiesTotalShareChangeDataSource = new MatTableDataSource<NumSharesSummaryItem>();
     this.ownershipSummaryDataSource = new MatTableDataSource<OwnershipSummaryItem>();
+    this.tradeSummarySignalDataSource = new MatTableDataSource<TradeSummarySignalItem>();
+    this.tradeSummarySignalDetailsDataSource =  new MatTableDataSource<TradeSummarySignalDetailsItem>();
 
     this.options = fb.group({
       hideRequired: false,
@@ -172,12 +211,15 @@ export class ShareholdingsComponent implements OnInit {
     this.companyField = new FormControl();
     this.shareholderField = new FormControl();
     this.equitiesTotalShareChangeDataSource.sort = this.equitiesTotalShareSort;
+    this.tradeSummarySignalDataSource.sort = this.tradesummarysignalSort;
+    this.tradeSummarySignalDetailsDataSource.sort = this.tradeSummarySignalDetailsSort;
+
     setTimeout(() => {
       this.ownershipSummaryDataSource.sort = this.ownershipSummarySort;
     });
 
 
-
+    // Fetch the NumShares Summary from firestore
     let items: Observable < NumSharesSummaryItem[] >;
 
     items = this.fireStore.collection<NumSharesSummaryItem>("/numsharessummary/Bursa/27-Feb-2019").valueChanges();
@@ -198,11 +240,98 @@ export class ShareholdingsComponent implements OnInit {
     ownershipItems.subscribe( arr => {
       this.ownershipSummaryDataSource.data = arr;
     });
+
+
+    // Fetch the Trade Summary Signal from  Firestore
+    let tradeSummarySignalItems: Observable<TradeSummarySignalItem[]>;
+
+    tradeSummarySignalItems = this.fireStore.collection<TradeSummarySignalItem>("/TradeSummarySignal/Bursa/27-Feb-2019/Aggregate_1/List").valueChanges();
+
+    tradeSummarySignalItems.subscribe(arr=> {
+      this.tradeSummarySignalDataSource.data = arr;
+    });
+
   }
 
   selectRow(row)
   {
      console.log('Row selected is ' + row.Symbol);
+  }
+
+  sortData(sort: Sort) {
+
+    let column = sort.active;
+console.log(sort.active);
+   console.log('Direction ' + sort.direction );
+
+
+
+    function compare(a,b)  {
+
+      let val = -1;
+      if(sort.direction == 'desc')
+      {
+        val = 1;
+      }
+      if (a[column] < b[column])
+        return val;
+      if (a[column] > b[column])
+        return val*(-1);
+      return 0;
+    }
+
+    let arr =  this.tradeSummarySignalDetailsDataSource.data;
+
+    arr.sort(compare);
+
+    this.tradeSummarySignalDetailsDataSource.data = arr;
+
+    /*
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'name': return compare(a.name, b.name, isAsc);
+        case 'calories': return compare(a.calories, b.calories, isAsc);
+        case 'fat': return compare(a.fat, b.fat, isAsc);
+        case 'carbs': return compare(a.carbs, b.carbs, isAsc);
+        case 'protein': return compare(a.protein, b.protein, isAsc);
+        default: return 0;
+      }
+    });
+
+    */
+  }
+
+  selectTradeSummarySignalRow(row)
+  {
+    this.tradeSummaryDetailSymbol = row.Symbol;
+    this.mostBuyUpPrice = row.MostBuyUpPrice;
+    this.mostSellDownPrice = row.MostSellDownPrice;
+    console.log('Trade Summary signal row is ' + row.Symbol);
+
+    //Sets the TradeSummarySignal details table
+    // /TradeSummarySignal/Bursa/27-Feb-2019/Aggregate_1/List/0127/List
+
+    let tradeSummarySignalDetailItems: Observable<TradeSummarySignalDetailsItem[]>;
+    let path = '/TradeSummarySignal/Bursa/27-Feb-2019/Aggregate_1/List/' +  row.Symbol +'/List';
+
+    tradeSummarySignalDetailItems = this.fireStore.collection<TradeSummarySignalDetailsItem>(path).valueChanges();
+
+    tradeSummarySignalDetailItems.subscribe(arr=> {
+
+      function compare(a,b) {
+        if (a.Price < b.Price)
+          return 1;
+        if (a.Price > b.Price)
+          return -1;
+        return 0;
+      }
+
+      arr.sort(compare);
+
+      this.tradeSummarySignalDetailsDataSource.data = arr;
+    });
+
   }
 
 }
